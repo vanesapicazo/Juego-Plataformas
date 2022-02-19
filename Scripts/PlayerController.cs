c25547706b3401a0e13981f7f14b1eacbc64952b
vanesa_picazo <vanesapicazoagusti@gmail.com>
2022-02-19 18:58:35 +0100

    Ejercicio 6

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D myRB;
+    public float speed;
+    bool faceRight = true;
+    SpriteRenderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (move < 0 && !faceRight)
+        {
+            Turn();
+        }
+        else
+        {
+            if(move < 0 && faceRight)
+            {
+                Turn();
+            }
+        }
+
+        myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
+    }
+
+    void Turn()
+    {
+        myRenderer.flipX = !myRenderer.flipX;
+        faceRight = !faceRight;
+    }
+}
